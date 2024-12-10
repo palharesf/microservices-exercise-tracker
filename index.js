03c5a5dede2ed21da29e15b39a29628599e7083d
Fernando <palhares.f@gmail.com>
2024-12-10 15:15:21 -0500

    Tests 9-15 passing, still need to adjust counting and filtering logics

--- index.js
+++ index.js
@@ -23,19 +23,28 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 
-// Define data structures: a Map with userId/username key-value pair, and an Object with Objects inside, each containing userId, description, duration, and date inside
+// Define data structures: a Map with userId/username key-value pair, and a Map with _id as a key, and the value being a collection of arrays, each represeting an exercise session
 const users = new Map();
-const exercises = {};
+const exercises = new Map();
 let idIndex = 0;
 let userId = "";
+let _id = "";
+const exerciseCountMap = new Map();
+
+function addExerciseSession(_id, exerciseSession) {
+  if (!exercises.has(_id)) {
+    exercises.set(_id, []);
+  }
+  exercises.get(_id).push(exerciseSession);
+}
 
 // Add a new user; no functionality to check for repeated usernames yet
 app.post('/api/users', (req, res) => {
   const username = req.body.username;
   idIndex++;
-  userId = idIndex.toString();
-  users.set(userId, username);  
-  res.json({ username: username, _id: userId });
+  _id = idIndex.toString();
+  users.set(_id, username);  
+  res.json({ username, _id });
 });
 
 // Get a list of all users
@@ -47,26 +56,54 @@ app.get('/api/users', (req, res) => {
 // Add a new exercise
 app.post('/api/users/:_id/exercises', (req, res) => {
   let date;
-  if(req.body.date) {
+  if (req.body.date) {
     date = new Date(req.body.date);
   } else {
     date = new Date();
   }
-
   dateStr = date.toUTCString();
   let parts = dateStr.split(' ');
   let rearrangedDate = `${parts[0].replace(',', '')} ${parts[2]} ${parts[1]} ${parts[3]}`;
 
-  res.json({
-    _id: req.params._id.toString(),
-    username: users.get(req.params._id),
+  _id = req.params._id;
+  username = users.get(_id);
+  duration = parseInt(req.body.duration);
+  description = req.body.description;
+
+  let exerciseSession = {
+    _id,
+    username,
     date: rearrangedDate,
-    duration: parseInt(req.body.duration),
-    description: req.body.description
+    duration,
+    description,
+  };
+
+  addExerciseSession(_id, exerciseSession);
+
+  res.json({
+    _id,
+    username,
+    date: rearrangedDate,
+    duration,
+    description,
   });
 });
 
 // Retrieve exercise log
-app.get('/api/users/:_id/logs?[from][&to][&limit]', (req, res) => {
+app.get('/api/users/:_id/logs', (req, res) => {
 
+  let from = req.query.from;
+  let to = req.query.to;
+  let limit = req.query.limit;
+
+  _id = req.params._id;
+  console.log("_id:", _id);
+  let log = exercises.get(_id) || [];
+  console.log("Log:", log);
+
+  res.json({
+    _id,
+    count: 12345,
+    log: log,
+   });
 });
